d657e69c483a2b8af481729c859327d065159a88
Georg Nagel <g.schlmm@gmail.com>
2021-06-04 19:52:08 +0200

    use the solution provided by @hivokas ( https://github.com/tailwindlabs/tailwindcss/discussions/3646#discussioncomment-826869 )

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 const fs = require('fs')
 const path = require('path')
-import commonjs from '@rollup/plugin-commonjs';
-
-const plugins = []
-if (process.env.NODE_ENV == 'production')
-  plugins.push(commonjs())
 
 export default {
-  plugins,
   resolve: {
     alias: {
       '@tailwindConfig': path.resolve(__dirname, 'tailwind.config.js'),
@@ -20,9 +14,7 @@ export default {
   },
   build: {
     commonjsOptions: {
-      include: [
-        'tailwind.config.js',
-      ],
+      transformMixedEsModules: true,
     }
   }
 }
