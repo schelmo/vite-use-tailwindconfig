dea7e65b43ac7a75d87a3b11e7ef71beb197cedf
Georg Nagel <g.schlmm@gmail.com>
2021-06-04 19:31:52 +0200

    use @rollup/plugin-commonjs when building

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 const fs = require('fs')
 const path = require('path')
+import commonjs from '@rollup/plugin-commonjs';
+
+const plugins = []
+if (process.env.NODE_ENV == 'production')
+  plugins.push(commonjs())
 
 export default {
+  plugins,
   resolve: {
     alias: {
       '@tailwindConfig': path.resolve(__dirname, 'tailwind.config.js'),
@@ -12,4 +18,11 @@ export default {
       '@tailwindConfig',
     ],
   },
+  build: {
+    commonjsOptions: {
+      include: [
+        'tailwind.config.js',
+      ],
+    }
+  }
 }
